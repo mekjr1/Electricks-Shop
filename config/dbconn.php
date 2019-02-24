7e60d0b1c444422273b2344b053fb31c9b3361cd
mekjr1 <gafar_meque@hotmail.com>
2019-02-24 20:48:42 +0200

    Commit for the first time to deploy

---
+++ config/dbconn.php
@@ -0,0 +1,11 @@
+<?php
+$dbconn = mysqli_connect("den1.mysql1.gear.host","root","Fv9up6?lf~nN","electricksdb");
+
+// Check connection
+if (mysqli_connect_errno())
+  {
+  echo "Failed to connect to MySQL: " . mysqli_connect_error();
+  }
+
+  date_default_timezone_set("Asia/Manila"); 
+?>
